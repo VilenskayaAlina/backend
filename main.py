68d74551dd65b366c494fab509816ac2d65a8ac8
VilenskayaAlina <vilen_alina2003@mail.ru>
2024-09-28 11:46:58 +0300

    Backend 2.0

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 class Data(BaseModel):
     data: str
+
+# Настройка CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Позволяет запросы с любых источников (для разработки, в продакшене лучше указать точный URL фронтенда)
+    allow_credentials=True,
+    allow_methods=["*"],  # Разрешить все HTTP-методы
+    allow_headers=["*"],  # Разрешить любые заголовки
+)
 
 @app.get("/")
 async def root():
@@ -19,3 +29,9 @@ async def save_data(item: Data):
     with open("data.txt", "a") as file:
         file.write(item.data + "\n")
     return {"message": "Data saved"}
+
+@app.get("/api/get-data")
+async def get_data():
+    with open("data.txt", "r") as file:
+        content = file.read()
+    return {"data": content}
